a5aeb2fb13bbc58b25c5a7fd4e22e29e2a4e18f0
Shivansh Pandey <shivanshp400@gmail.com>
2022-06-26 16:42:06 +0530

    first commit from tester 2

---
+++ src/test/java/project1/tester2.java
@@ -0,0 +1,10 @@
+package project1;
+
+public class tester2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hey i am tester 2 and i would love to collaborate with you too man");
+	}
+
+}
