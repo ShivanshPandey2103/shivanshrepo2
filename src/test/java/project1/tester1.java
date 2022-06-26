4ae4cc624144c0bb39d0c92f0404c78c67b084bb
Shivansh Pandey <shivanshp400@gmail.com>
2022-06-26 16:37:07 +0530

    first commit from tester 1

---
+++ src/test/java/project1/tester1.java
@@ -0,0 +1,10 @@
+package project1;
+
+public class tester1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("hey i am a new tester joining the company and i wanna collaborate");
+	}
+
+}
