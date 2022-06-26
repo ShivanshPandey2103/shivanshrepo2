06187bdcc2aa510b4ccce0818802256301f5eb35
Shivansh Pandey <shivanshp400@gmail.com>
2022-06-26 16:12:26 +0530

    new commit from Shivansh Pandey

---
+++ src/test/java/project1/democlass.java
@@ -0,0 +1,12 @@
+package project1;
+
+public class democlass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Shivansh Pandey");
+
+	}
+
+}
